405e09696158565880c4416e7f6c936d8684f48c
Sanjiv Kumar <sanjiv@ccs.neu.edu>
2018-05-24 15:21:39 -0400

    New Angular Cli project for material typeahead directive library

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('mat-ta-root h1')).getText();
+  }
+}
